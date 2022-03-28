79335baef92c20b7820a4e8d679257965ffef5df
Eddy Ruiz Azcuy <eddyraz.fl@gmail.com>
2022-03-28 10:03:28 -0400

    ajustando valores para el supervisor

--- lib/gestor_workers.ex
+++ lib/gestor_workers.ex
@@ -1,5 +1,7 @@
 defmodule SupervisorDeProcesos do
 
+  @puerto 9000
+
   def start do
     children = [ {EchoClient, []}]
     Supervisor.start_link(children, name: :supervisor_de_procesos, strategy: :one_for_one)
